9f14317e2f53ccd0b8eccd1b8a0aac4aa0b83df0
sasha <sasha@debianmain>
2023-07-30 21:35:31 +0300

    comment

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "!2@_3(e6%tpp-vhe3fu56^5y4w3#ib3tr5i#nrim(wg=a76%=t"
+
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local import *  # noqa
+except ImportError:
+    pass
